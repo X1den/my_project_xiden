95c68ea209e2001dd4dc4053d4f2d810b5e54a78
X1den <52957409+X1den@users.noreply.github.com>
2025-09-01 22:39:01 +0400

    new function + close_game

--- snake/snake.py
+++ snake/snake.py
@@ -1,5 +1,9 @@
 import turtle
 import time
+import random 
+
+delay = 0.1
+score = 0
 
 #Настройки экрана
 win = turtle.Screen()
@@ -16,6 +20,31 @@ head.shape("square")
 head.color("green")
 head.penup()
 head.direction = "stop"
+
+#Еда
+food = turtle.Turtle()
+food.speed(0)
+food.shape("circle")
+food.color("red")
+food.penup()
+food.goto(0,100)
+
+#Тело
+segments = []
+
+#Счет
+pen = turtle.Turtle()
+pen.speed(0)
+pen.color("white")
+pen.penup()
+pen.hideturtle()
+pen.goto(0,260)
+pen.write("Счет: 0", align="center", font=("Courier", 24, "normal"))
+
+def close_game():
+    if head.direction != "stop":
+        head.direction = "stop"
+        win.bye()
 
 def go_up():
     if head.direction != "down":
@@ -55,8 +84,64 @@ win.onkeypress(go_up, "Up")
 win.onkeypress(go_down, "Down")
 win.onkeypress(go_left, "Left")
 win.onkeypress(go_right, "Right")
+win.onkeypress(close_game, "q")
 
 while True:
     win.update()
+
+    if head.xcor() > 290 or head.xcor() < -290 or head.ycor() > 290 or head.ycor() < -290:
+        time.sleep(1)
+        head.goto(0, 0)
+        head.direction = "stop"
+
+        for segment in segments:
+            segment.goto(1000, 1000)
+        segments.clear()
+        score = 0
+        pen.clear()
+        pen.write("Счёт: {}".format(score), align="center", font=("Courier", 24, "normal"))
+
+    if head.distance(food) < 20:
+        # Перемещаем еду
+        x = random.randint(-290, 290)
+        y = random.randint(-290, 290)
+        food.goto(x, y)
+
+        new_segment = turtle.Turtle()
+        new_segment.speed(0)
+        new_segment.shape("square")
+        new_segment.color("darkgreen")
+        new_segment.penup()
+        segments.append(new_segment)
+
+        # Увеличиваем счёт
+        score += 10
+        pen.clear()
+        pen.write("Счёт: {}".format(score), align="center", font=("Courier", 24, "normal"))
+
+    for i in range(len(segments)-1, 0, -1):
+        x = segments[i-1].xcor()
+        y = segments[i-1].ycor()
+        segments[i].goto(x, y)
+
+    if len(segments) > 0:
+        x = head.xcor()
+        y = head.ycor()
+        segments[0].goto(x, y)
+
     move()
-    time.sleep(0.1)
+
+    for segment in segments:
+        if segment.distance(head) < 20:
+            time.sleep(1)
+            head.goto(0, 0)
+            head.direction = "stop"
+
+            for segment in segments:
+                segment.goto(1000, 1000)
+            segments.clear()
+            score = 0
+            pen.clear()
+            pen.write("Счёт: {}".format(score), align="center", font=("Courier", 24, "normal"))
+
+    time.sleep(delay)
